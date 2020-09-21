c80c377afdf4829fbf72b2b8b269b85bd4a4e3de
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-21 02:34:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,3 @@ end
 
 
 
-oppressed_workers = ["dopey", "sneezy", "happy", "angry", "doc", "lemonjello", "sleepy" ]
-oppressed_workers.each do |oppressed_worker|
-   puts "#{oppressed_worker.capitalize} wants to start a union!"
-end #=>
